f61da6c02bb9ba0a0b19e56e523dc7914e5cc902
Ezhilananthsakthivel <71553660+Ezhilananthsakthivel@users.noreply.github.com>
2023-03-18 12:05:27 +0530

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -43,7 +43,7 @@ navLinks.forEach((n) => n.addEventListener("click", () => { navMenu.classList.re
 const sr = ScrollReveal({
   origin: "top",
   distance: "60px",
-  duration: 1500,
+  duration: 1800,
   reset: true,
 });
 
